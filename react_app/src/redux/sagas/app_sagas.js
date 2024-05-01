d1fa5bdd7f838e83eac96025c601479146e06ad8
StevenB96 <62147124+StevenB96@users.noreply.github.com>
2024-05-01 18:44:39 +0100

    Added redux and component boilerplate.

--- react_app/src/redux/sagas/app_sagas.js
+++ react_app/src/redux/sagas/app_sagas.js
@@ -6,54 +6,10 @@ import {
 } from 'redux-saga/effects';
 
 import {
-  getProfileSuccess,
-  getProfileFailure,
-
-  getContentSuccess,
-  getContentFailure,
-} from '../actions/app_actions';
-
-import {
-  GET_PROFILE_REQUEST,
-  GET_CONTENT_REQUEST,
 } from '../actions/app_actions';
 
 import { fetchDataFromApi } from '../api';
 import env from '../../env';
 
-function* getProfile(action) {
-  try {
-    const data = yield call(fetchDataFromApi, {
-      method: 'GET',
-      url: `${env.SERVER_URL || "http://localhost:3000"}/`,
-      path: 'profile/',
-      qParams: '',
-      payload: action.payload
-    });
-
-    yield put(getProfileSuccess(data));
-  } catch (error) {
-    yield put(getProfileFailure(error.message));
-  }
-}
-
-function* getContent(action) {
-  try {
-    const data = yield call(fetchDataFromApi, {
-      method: 'GET',
-      url: `${env.SERVER_URL || "http://localhost:3000"}/`,
-      path: 'content/',
-      qParams: '',
-      payload: action.payload
-    });
-
-    yield put(getContentSuccess(data));
-  } catch (error) {
-    yield put(getContentFailure(error.message));
-  }
-}
-
 export default function* app_sagas() {
-  yield takeLatest(GET_PROFILE_REQUEST, getProfile);
-  yield takeLatest(GET_CONTENT_REQUEST, getContent);
 }
